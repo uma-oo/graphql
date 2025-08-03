e95722936698839cbfe00d810b274ca5a5c700c0
uma-oo <oumayma.elfahsi99@gmail.com>
2025-08-03 11:55:47 +0100

    css never fails to make feel angry

--- js/pages/homePage.js
+++ js/pages/homePage.js
@@ -2,6 +2,7 @@
 import { fetchData } from '../api/api.js'
 import { Home } from '../components/home.js'
 import { queryData } from '../queries/groups.js'
+import { BarChart } from '../svg/barChart.js'
 import { PieChartSkills } from '../svg/pieChart.js'
 
 export async function renderHomePage(app, userData) {
@@ -17,15 +18,15 @@ export async function renderHomePage(app, userData) {
     const personalInfo = Object.values(dataOrganized[0])[0];
     const level = Object.values(dataOrganized[1])[0];
     const cohorts = Object.values(dataOrganized[2])[0];
-    const projects = Object.values(dataOrganized[3])[0];
+    const projects = Object.values(dataOrganized[3]);
+    console.log("projects", projects);
     const skills = Object.values(dataOrganized[4])[0];
-    console.log(skills);
     const main = document.querySelector("main")
     // let's handle here the bar chart svg  w logic ;) 
     // the hardest part 
+    const barChartContainer = BarChart(projects)
 
-    
-
+    main.append(barChartContainer)
 
     // handle the pie chart logic 
     const [skillsChartContainer, buttons] = PieChartSkills(skills)
--- js/svg/barChart.js
+++ js/svg/barChart.js
@@ -7,22 +7,86 @@
 // ila zd9 hadshi ana safi ghanwli front end engineer u blasti mashi hna 
 
 
+import { createSvgElement, setSvgAttributes, createElement } from "../utils/utils.js"
+
+
 // horizontal one 7itash projects name are long and we need to display them all
 export function BarChart(projectsData) {
-
-
-}
-
-
-
-// helper function to calclulate dimensions of the bar chart
-// ghaliban ghaid nkhdm b log bash data tkuun mgadaaa
-
-function calculateBarDimensions(projectsData) {
-    maxXP = Math.max(...projectsData.map(project => project.xp_per_project.transactions[0].amount));
+    const barChartContainer = createElement('div', 'bar-chart-container');
+    const maxXP = Math.max(...projectsData.map(project => project.xp_per_project.transactions[0].amount));
     // to handle the width of the bar chart
-    chartWidth = 600
-    labelWidth = 150
-    maxBarWidth = chartWidth - labelWidth
+    const chartWidth = 600
+    const labelWidth = 250
+    const maxBarWidth = chartWidth - labelWidth
+    // the bar width will be calculated based on the maxXP 
     // to handle the height of the bar chart
+    const barHeight = 40
+    const gap = 20 // gap between bars
+    const chartHeight = projectsData.length * barHeight + gap
+
+    // let's sort the data by the name of the project
+    projectsData.sort((a, b) => a.name_project.name.localeCompare(b.name_project.name));
+    console.log("sorted projectsData", projectsData);
+
+    const svg = createSvgElement('svg')
+    setSvgAttributes(svg, {
+        width: chartWidth,
+        height: chartHeight,
+        class: 'bar-chart'
+    });
+    projectsData.forEach((project, index) => {
+        // 
+        const teamMembers = project.members_aggregate.team;
+        let teamMemberNames = []
+        teamMembers.map(
+            (member) => {
+                teamMemberNames.push(member.userLogin)
+            }
+        );
+        console.log(teamMemberNames);
+        const barWidth = (project.xp_per_project.transactions[0].amount / maxXP) * maxBarWidth;
+        const yPosition = (index * barHeight) + gap; // to add a gap between bars
+        //  nssit l grp ;)
+        const group = createSvgElement('g');
+        setSvgAttributes(group, {
+            class: 'bar-group'
+        })
+        // Create a rectangle for the bar
+        const rect = createSvgElement('rect');
+        setSvgAttributes(rect, {
+            x: labelWidth,
+            y: yPosition,
+            width: barWidth,
+            height: barHeight - 5,
+            class: 'bar',
+            rx: 8,
+        });
+    
+
+        // Create a text element for the project name
+        const text = createSvgElement('text');
+        setSvgAttributes(text, {
+            x: 10,
+            y: yPosition + barHeight / 2 + 5,
+            fill: 'var(--fourth-color)',
+            class: 'bar-label'
+        });
+        text.textContent = project.name_project.name;
+        const divTitle = createElement('div', 'bar-title');
+        const title = createSvgElement('text');
+        setSvgAttributes(title, { class: 'bar-text', x: labelWidth, y: yPosition + barHeight / 2 + 5 });
+        title.textContent = `Team Members: ${teamMemberNames.join(', ')} XP: ${project.xp_per_project.transactions[0].amount}`;
+        divTitle.appendChild(title);
+        
+        group.append(rect, text, divTitle);
+        svg.appendChild(group);
+    });
+
+
+    barChartContainer.appendChild(svg);
+    return barChartContainer;
+
+
 }
+
+
