900ef03f440004fd118c85266f6a4cbed4b04f3d
uma-oo <oumayma.elfahsi99@gmail.com>
2025-08-05 00:46:43 +0100

    add the footer for the version before the last one

--- js/components/home.js
+++ js/components/home.js
@@ -1,11 +1,16 @@
 
-import { createElement } from "../utils/utils.js";
+import { createElement, setAttributes } from "../utils/utils.js";
 
 export function Home(app) {
     const header = createElement('header', 'header-section', '')
     const main = createElement('main', 'main-section')
     const footer = createElement('footer', 'footer-section', '')
-   
+    const footerContainer = createElement('div', 'footer-container')
+    const footerContent = createElement('span', null , 'Created with hardships, struggles and a little bit of love 🌸 by')
+    const githubLink = createElement('a', 'link', 'uma-oo')
+    setAttributes(githubLink, {'href': 'https://github.com/uma-oo', 'target': '_blank'})
+    footerContainer.append(footerContent, githubLink)
+    footer.append(footerContainer)
     app.append(header, main, footer)
     return app
 }
