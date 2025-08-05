ff6ef0442591470aa351ace2d4e74745e8470743
uma-oo <oumayma.elfahsi99@gmail.com>
2025-08-05 18:54:53 +0100

    add some code

--- js/components/header.js
+++ js/components/header.js
@@ -5,7 +5,7 @@ import { renderApp } from "../index.js";
 
 export function Header(header, data) {
     const greeting = createElement('h1', 'greeting', `Welcome back ${data.login}`)
-    const logoutButton = createButton({ text: "Log out" }, 'submit', 'button-skills logout')
+    const logoutButton = createButton({ text: "Log-out" }, 'submit', 'button-skills logout')
     logoutButton.addEventListener('click', (() => {
         localStorage.removeItem("token")
         renderApp()
